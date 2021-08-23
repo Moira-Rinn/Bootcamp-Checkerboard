68c59c418646db1f043ebb939ab902f374c9daa9
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-08-23 02:54:36 -0400

    Final commit for now. Extra functionality abandoned indefinately.

--- Checkerboard.py
+++ Checkerboard.py
@@ -16,12 +16,6 @@ def boardX(y, x=8):
 def boardXy(x, y):
     return render_template('board.html', x=int(x), y=int(y))
 
-# To be implemented:
-
-# @app.route('/<x>/<y>/<color1>/<color2>')
-# def contact(x, y, color1=[12, 11, 11, 0.4], color2=[136, 0, 0, 0.4]):
-#     return render_template('repeat.html', x=int(x), y=int(y), color1=list(color1), color2=list(color2))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
